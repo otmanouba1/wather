721bd256e40904d896faaf8bb01d5d806479bde3
Atmane oubayoucef <otmanouba2@gmail.com>
2023-12-05 21:01:24 +0100

    still working

---
+++ wather.js
@@ -0,0 +1,20 @@
+const input = document.querySelector(".input");
+const watherIcons = {
+  rain: "./icons/sun.png",
+  cloudy: "./icons/raining",
+  "clear-day": "./icons/cloudy (1).png",
+  Clear: "./icons/cloudy (2).png",
+};
+
+const wather = () => {
+  const url =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/londona?unitGroup=metric&key=QW7V7T2YZ54RJF6VSQGGBWUTQ&contentType=json";
+  fetch(url, {
+    method: "GET",
+    headers: {},
+  }).then((response) => {
+    console.log(response);
+  });
+};
+
+wather();
