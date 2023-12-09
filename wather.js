b24de2d72dfe1681f86186a5e5cbebe40acf89de
otmanouba1 <otmanouba2@gmail.com>
2023-12-09 16:57:35 +0100

    just workin

--- wather.js
+++ wather.js
@@ -1,16 +1,10 @@
 const input = document.querySelector(".input");
 const temp = document.querySelector(".temp");
 const hourlyTemp = document.querySelector(".hourly-temp");
-const watherIcons = {
-  'rain': "./icons/sun.png",
-  "partly-cloudy-day": "./icons/raining",
-  "cloudy": "./icons/cloudy (1).png",
-  'Clear': "./icons/cloudy (2).png",
-};
+const filesLike =document.querySelector('.files-like')
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    // console.log("hohoh");
     wather(input.value);
   }
 });
@@ -19,14 +13,15 @@ const wather = (cityname) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => editHtml(data));
+    // console.log(data);
 };
 const editHtml = (data) => {
-  const watherIconType =JSON.stringify( data.currentConditions.icon)
-  console.log(watherIconType);
+  // const watherIconType =JSON.stringify( data.currentConditions.icon)
+  console.log(data.currentConditions.icon);
 
   temp.innerHTML = `
     <h1>${data.resolvedAddress}</h1>
-    <img src="${watherIcons.watherIconType}" alt="" />
+    <img src="iconses/${data.currentConditions.icon}.svg" alt="" />
     <div class="flex">
        <div> <h3>temp</h3>
         <p>${data.currentConditions.temp}</p>
@@ -47,4 +42,57 @@ const editHtml = (data) => {
       <div><p>${data.description}</p></div>
     </div>
     `;
+    hourlyTemp.innerHTML=''
+    for (let i = 0; i < data.days[0].hours.length; i++) {
+      hourlyTemp.innerHTML+= `<div>
+      <p>${data.days[0].hours[i].datetime}</p>
+      <img src="iconses/${data.days[0].hours[i].icon}.svg" alt="" />
+    </div>`
+    if(data.days[0].hours.length>=20){
+      i+=6
+      
+    } else if(data.days[0].hours.length>=10){
+      i+=3
+      
+    }
+      
+    }
+    filesLike.innerHTML=''
+    for (let i = 0; i < data.days[0].hours.length; i++) {
+   
+      
+      filesLike.innerHTML+=`
+      <div>  <p>${data.days[0].hours[i].datetime}</p>
+       <h5>files like</h5>
+       <p>${data.days[0].hours[i].feelslike}</p></div>`
+       if(data.days[0].hours.length>=20){
+        i+=6
+        
+      } else if(data.days[0].hours.length>=10){
+        i+=3
+        
+      }
+      
+}
+   
+    // console.log(data.days[0].hours.length);
 };
+
+
+
+
+
+
+
+
+// `
+//     <div>
+//     <p>${data.days[0].hours[0].datetime}</p>
+//     <img src="iconses/${data.days[0].hours[0].icon}.svg" alt="" />
+//   </div>
+//   <div>
+//     <p>${data.days[0].hours[3].datetime}</p>
+//     <img src="iconses/${data.days[0].hours[3].icon}.svg" alt="" />
+//   </div>
+ 
+//     `
